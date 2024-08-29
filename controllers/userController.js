583a339a16fcab7ffe7a766c36eee74d10fcbf97
Shivam-Singh0 <ssshivamsingh151@gmail.com>
2024-08-29 16:03:50 +0530

    logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,6 +74,6 @@ export const logOut = async (req, res) => {
     httpOnly: true,
     expires: new Date(0),
   });
-  res.status(200).json({ message: "Logged out successfully" });
+  res.status(200).json({ message: "Logged out successfully"});
 };
 
