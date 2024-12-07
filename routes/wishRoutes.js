1e36bb35e461ada019290d48bfe6b366e16f9e82
Shivam-Singh0 <ssshivamsingh151@gmail.com>
2024-12-07 22:14:01 +0530

    wishlist

--- routes/wishRoutes.js
+++ routes/wishRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addToWishlist, getWishlist, removeWishlist } from "../controllers/wishListController.js";
+import { addToWishlist, getWishlist, isWishlisted, removeWishlist } from "../controllers/wishListController.js";
 import authenticateUser from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("").get(authenticateUser,getWishlist).post(authenticateUser, addToWishlist).delete(authenticateUser, removeWishlist);
+router.route("is-wishlisted").post(authenticateUser, isWishlisted)
 
 export default router;
