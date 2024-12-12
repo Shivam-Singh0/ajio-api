a50e4fbc6f9c90887363d74ab0e1eb2b85d53882
Shivam-Singh0 <ssshivamsingh151@gmail.com>
2024-12-12 19:16:58 +0530

    address

--- controllers/AddressController.js
+++ controllers/AddressController.js
@@ -4,7 +4,7 @@ import Address from "../models/Address.js";
 
 export const addAdress = async(req, res) => {
     const userId = req.user.uid;
-    console.log(req.body)
+   
     const address = await Address.findOne({userId});
     if (address) {
         try {
