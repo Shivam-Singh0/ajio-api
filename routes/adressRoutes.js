d8ab6727a0a7927880b64ffa82a0c3c425da4f1e
Shivam-Singh0 <ssshivamsingh151@gmail.com>
2024-12-13 19:12:26 +0530

    address

--- routes/adressRoutes.js
+++ routes/adressRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import authenticateUser from "../middleware/authMiddleware.js";
-import { addAdress } from "../controllers/AddressController.js";
+import { addAdress, getAddress } from "../controllers/AddressController.js";
 
 const router = express.Router();
-router.route("").post(authenticateUser, addAdress);
+router.route("").get(authenticateUser, getAddress).post(authenticateUser, addAdress);
 export default router;
